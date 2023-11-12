075545ccba7fc307f11812d38d1b349a14d373ee
CMOISDEAD <camilodavina@gmail.com>
2023-11-12 13:25:36 -0500

    netlify integration

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,9 +1,10 @@
 import cors from "cors";
-import express, { Express } from "express";
-import apiRouter from "./routes/api/provider";
-import pdfRouter from "./routes/pdf/provider";
-import authRouter from "./routes/auth/provider";
 import * as dotenv from "dotenv";
+import express, { Express } from "express";
+import serverless from "serverless-http";
+import apiRouter from "../../src/routes/api/provider";
+import pdfRouter from "../../src//routes/pdf/provider";
+import authRouter from "../../src//routes/auth/provider";
 
 dotenv.config();
 
@@ -20,6 +21,4 @@ app.use("/api", apiRouter);
 app.use("/pdf", pdfRouter);
 app.use("/auth", authRouter);
 
-app.listen(app.get("port"), () => {
-  console.log(`Server running on port ${app.get("port")}`);
-});
+export const handler = serverless(app);
