ee05a5308e6b000fa0d5d0566cf79bfb2fccd1aa
CMOISDEAD <camilodavina@gmail.com>
2023-11-12 15:20:07 -0500

    auth test endpoint

--- src/routes/auth/provider.ts
+++ src/routes/auth/provider.ts
@@ -7,5 +7,8 @@ router.post("/login", login);
 router.post("/register", register);
 router.post("/recover", recover);
 router.post("/buyMembership", buyMembership);
+router.get("/test", (req, res) => {
+  res.status(200).json({ message: "Auth route" });
+});
 
 export default router;
