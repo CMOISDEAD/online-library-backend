20ef0efe8eb51bb9a38082f8b7d852b4330d3657
CMOISDEAD <camilodavina@gmail.com>
2023-11-12 14:02:04 -0500

    try with vercel

---
+++ api/index.ts
@@ -0,0 +1,23 @@
+import cors from "cors";
+import express, { Express } from "express";
+import apiRouter from "../src/routes/api/provider";
+import pdfRouter from "../src/routes/pdf/provider";
+import authRouter from "../src/routes/auth/provider";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(cors());
+
+app.use("/api", apiRouter);
+app.use("/pdf", pdfRouter);
+app.use("/auth", authRouter);
+
+export default app;
